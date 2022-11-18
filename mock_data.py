a3d2d3421048cdf5a0d26b6697e9a8d3a7f962fb
mcc808 <112995753+mcc808@users.noreply.github.com>
2022-11-17 20:05:44 -0800

    end points API

---
+++ mock_data.py
@@ -0,0 +1,67 @@
+catalog = [
+
+        {
+        "_id": "12389mxie",
+        "title": "Cups",
+        "desc": "Pack of 100. Recycled material.",
+        "price": 11.99,
+        "category": "supply",
+        "image":"cups.jpg"
+    },
+
+    {
+        "_id": "12555lpqa",
+        "title": "Filters",
+        "desc": "Pack of 50 standard filters as needed. ",
+        "price": 7.99,
+        "category": "supply",
+        "image":"filters.jpg"
+    },
+
+    {
+        "_id": "12809xcwe",
+        "title": "Mixer",
+        "desc": "Top-notch equipment for quick service.",
+        "price": 15.85,
+        "category": "equipment",
+        "image":"mixer.jpg"
+    },
+
+    {
+        "_id": "89234dckt",
+        "title": "Apron",
+        "desc": "Styling for baristas only.",
+        "price": 8.59,
+        "category": "accessories",
+        "image":"apron.jpg"
+    },
+
+    {
+        "_id": "90233tyor",
+        "title": "Grinder",
+        "desc": "Essential for fast production.",
+        "price": 20.33,
+        "category": "equipment",
+        "image":"grinder.jpg"
+    },
+
+    {
+        "_id": "67851hjdk",
+        "title": "Station",
+        "desc": "Stay organized at all times.",
+        "price": 12.59,
+        "category": "equipment",
+        "image":"station.jpg"
+    },
+
+    {
+        "_id": "74856nerf",
+        "title": "Kcups",
+        "desc": "A variety of 40 flavors.",
+        "price": 7.75,
+        "category": "supply",
+        "image":"kcup.jpg"
+    }
+
+
+]
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import json 
 from config import me
+from mock_data import catalog 
 
 app = Flask("server")
 
@@ -36,9 +37,79 @@ def version():
 #get /api/about
 #return me as jason
 
-@app.get("/about")
-def about():
+@app.get("/api/about")
+def api_about():
     return json.dumps(me)  
+
+
+
+@app.get("/api/catalog")
+def get_catalog():
+    return json.dumps(catalog)
+
+
+@app.get("/api/test/count")
+def nums_catalog():
+   return len(catalog)
+
+
+@app.get("/api/catalog/<category>")
+def by_category(category):
+    results = []
+    category = category.lower()
+    for product in catalog:
+        if product["category"].lower() == category:
+            results.append(product)
+        
+    return json.dumps(results)
+
+
+
+@app.get("/api/catalog/search/<text>")
+def search_by_text(text):
+    text = text.lower()
+    results = []
+
+    for product in catalog:
+        if text in product["title"].lower() or text in product["category"].lower():
+            results.append(product)
+
+    return json.dumps(results)
+
+
+
+@app.get("/api/categories")
+def find_categories():
+    
+    allCategories = []
+    for product in catalog:
+        cats = product["category"]
+        if cats not in allCategories:
+            allCategories.append(cats)
+    
+    return json.dumps(allCategories)
+
+
+
+@app.get("/api/test/value")
+def sum_goods():
+
+    total = 0
+    for product in catalog:
+        total = total + product["price"]
+
+    return json.dumps(total)
+
+
+
+@app.get("/api/product/<id>")
+def find_product_by_id(id):
+    for product in catalog:
+        if product["_id"] == id:
+            return json.dumps(product)
+
+    return "Error: Product was not found"
+
 
 
 app.run(debug=True)
